e4985b81b739a4bbd7d3061cefeaee98f94d0f07
samiraTR <samira.rashid.transcom@gmail.com>
2023-03-15 17:35:16 +0600

    rx Submit provider, repo, added

--- lib/services/rx/rx_apis.dart
+++ lib/services/rx/rx_apis.dart
@@ -2,4 +2,7 @@ class RxApis {
   static syncRxItemApi(
           String syncUrl, String cid, String userId, String userpass) =>
       '${syncUrl}api_medicine/get_rx_medicine?cid=$cid&user_id=$userId&user_pass=$userpass';
+
+  static rxSubmitApi(String submitUrl) =>
+      '${submitUrl}api_rx_submit/submit_data';
 }

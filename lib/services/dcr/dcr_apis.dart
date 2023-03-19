292b65513a8a962c4c4fb570c2d62d25cbb30851
tahmina-bristy-b2 <tahmina.bristy.transcom@gmail.com>
2023-03-19 17:27:15 +0600

    one line  Added in doctor page by Bristy

--- lib/services/dcr/dcr_apis.dart
+++ lib/services/dcr/dcr_apis.dart
@@ -49,7 +49,7 @@ class DcrApis {
           String fDocAddress,
           String brand,
           String dob) =>
-      'http://w03.yeapps.com/skf_api/api_doctor/doctor_add?cid=$skf&user_id=$userId&user_pass=$password&area_id=$areaId&area_name=$areaName&doc_name=$doctorName&category=$category&doctors_category=$doctorCategory&doctor_type=$doctorType&specialty=$specialty&degree=$degree&chemist_id=$chemistId&address=$draddress&district=$drDistrict&thana=$drThana&mobile=$drMobile&mar_day=$marDay&dob_child1=$child1&dob_child2=$child2&collar_size=$collerSize&nop=$nop&four_p_doc_id=$fDrId&fourP_doc_name=$fDrName&fourP_doc_specialty=$fDrspecilty&fourP_doc_address=$fDocAddress&brand=$brand&dob=$dob';
+      '$addUrl/doctor_add?cid=$skf&user_id=$userId&user_pass=$password&area_id=$areaId&area_name=$areaName&doc_name=$doctorName&category=$category&doctors_category=$doctorCategory&doctor_type=$doctorType&specialty=$specialty&degree=$degree&chemist_id=$chemistId&address=$draddress&district=$drDistrict&thana=$drThana&mobile=$drMobile&mar_day=$marDay&dob_child1=$child1&dob_child2=$child2&collar_size=$collerSize&nop=$nop&four_p_doc_id=$fDrId&fourP_doc_name=$fDrName&fourP_doc_specialty=$fDrspecilty&fourP_doc_address=$fDocAddress&brand=$brand&dob=$dob';
 
   static String docSettingsApi =
       'http://w03.yeapps.com/skf_api/api_doctor/get_doctor_setting_sync';

a583807c5bae65ad01345236b1646f5ca751b3b2
alaska-bear <pulture.transcom@gmail.com>
2023-04-04 16:04:02 +0600

    Promo

--- lib/models/promo_model.dart
+++ lib/models/promo_model.dart
@@ -12,26 +12,87 @@ String promoModelToJson(PromoModel data) => json.encode(data.toJson());
 class PromoModel {
   PromoModel({
     required this.status,
-    required this.promoList,
+    required this.promoPbList,
+    required this.promoDiList,
+    required this.promoFlatList,
+    required this.promoSpList,
   });
 
   final String status;
-  final List<PromoList> promoList;
+  final List<PromoPbList> promoPbList;
+  final List<PromoDiList> promoDiList;
+  final List<PromoFlatList> promoFlatList;
+  final List<PromoSpList> promoSpList;
 
   factory PromoModel.fromJson(Map<String, dynamic> json) => PromoModel(
         status: json["status"],
-        promoList: List<PromoList>.from(
-            json["promoList"].map((x) => PromoList.fromJson(x))),
+        promoPbList: List<PromoPbList>.from(
+            json["promoPbList"].map((x) => PromoPbList.fromJson(x))),
+        promoDiList: List<PromoDiList>.from(
+            json["promoDiList"].map((x) => PromoDiList.fromJson(x))),
+        promoFlatList: List<PromoFlatList>.from(
+            json["promoFlatList"].map((x) => PromoFlatList.fromJson(x))),
+        promoSpList: List<PromoSpList>.from(
+            json["promoSpList"].map((x) => PromoSpList.fromJson(x))),
       );
 
   Map<String, dynamic> toJson() => {
         "status": status,
-        "promoList": List<dynamic>.from(promoList.map((x) => x.toJson())),
+        "promoPbList": List<dynamic>.from(promoPbList.map((x) => x.toJson())),
+        "promoDiList": List<dynamic>.from(promoDiList.map((x) => x.toJson())),
+        "promoFlatList":
+            List<dynamic>.from(promoFlatList.map((x) => x.toJson())),
+        "promoSpList": List<dynamic>.from(promoSpList.map((x) => x.toJson())),
       };
 }
 
-class PromoList {
-  PromoList({
+class PromoDiList {
+  PromoDiList({
+    required this.promoDes,
+    required this.approvedDate,
+  });
+
+  final String promoDes;
+  final DateTime approvedDate;
+
+  factory PromoDiList.fromJson(Map<String, dynamic> json) => PromoDiList(
+        promoDes: json["promo_des"],
+        approvedDate: DateTime.parse(json["approved_date"]),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "promo_des": promoDes,
+        "approved_date":
+            "${approvedDate.year.toString().padLeft(4, '0')}-${approvedDate.month.toString().padLeft(2, '0')}-${approvedDate.day.toString().padLeft(2, '0')}",
+      };
+}
+
+class PromoFlatList {
+  PromoFlatList({
+    required this.promoDes,
+    required this.minQty,
+    required this.flatRate,
+  });
+
+  final String promoDes;
+  final int minQty;
+  final double flatRate;
+
+  factory PromoFlatList.fromJson(Map<String, dynamic> json) => PromoFlatList(
+        promoDes: json["promo_des"],
+        minQty: json["min_qty"],
+        flatRate: json["flat_rate"]?.toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "promo_des": promoDes,
+        "min_qty": minQty,
+        "flat_rate": flatRate,
+      };
+}
+
+class PromoPbList {
+  PromoPbList({
     required this.promoDes,
     required this.minQty,
   });
@@ -39,13 +100,37 @@ class PromoList {
   final String promoDes;
   final int minQty;
 
-  factory PromoList.fromJson(Map<String, dynamic> json) => PromoList(
-        promoDes: json["promo_des"] ?? '',
-        minQty: json["min_qty"] ?? 0,
+  factory PromoPbList.fromJson(Map<String, dynamic> json) => PromoPbList(
+        promoDes: json["promo_des"],
+        minQty: json["min_qty"],
       );
 
   Map<String, dynamic> toJson() => {
         "promo_des": promoDes,
         "min_qty": minQty,
+      };
+}
+
+class PromoSpList {
+  PromoSpList({
+    required this.promoDes,
+    required this.minQty,
+    required this.specialRateTp,
+  });
+
+  final String promoDes;
+  final int minQty;
+  final double specialRateTp;
+
+  factory PromoSpList.fromJson(Map<String, dynamic> json) => PromoSpList(
+        promoDes: json["promo_des"],
+        minQty: json["min_qty"],
+        specialRateTp: json["special_rate_tp"]?.toDouble(),
+      );
+
+  Map<String, dynamic> toJson() => {
+        "promo_des": promoDes,
+        "min_qty": minQty,
+        "special_rate_tp": specialRateTp,
       };
 }
--- lib/ui/promo_page.dart
+++ lib/ui/promo_page.dart
@@ -2,7 +2,6 @@ import 'package:MREPORTING/local_storage/boxes.dart';
 import 'package:MREPORTING/models/hive_models/dmpath_data_model.dart';
 import 'package:MREPORTING/models/hive_models/login_user_model.dart';
 import 'package:MREPORTING/models/promo_model.dart';
-import 'package:MREPORTING/services/all_services.dart';
 import 'package:MREPORTING/services/others/repositories.dart';
 import 'package:flutter/material.dart';
 import 'package:data_table_2/data_table_2.dart';
@@ -61,23 +60,58 @@ class _PromoPageState extends State<PromoPage> {
               );
             } else if (snapshot.connectionState == ConnectionState.done) {
               if (snapshot.hasError) {
-                AllServices().toastMessage(
-                    'Data Not found!', Colors.red, Colors.white, 16);
-                return const Text('');
-              } else if (snapshot.data!.promoList.isNotEmpty) {
-                return Padding(
-                  padding: const EdgeInsets.all(3),
-                  child: DataTable2(
-                    border: TableBorder.all(),
-                    headingRowColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.blue.shade200),
-                    dataRowColor: MaterialStateColor.resolveWith(
-                        (states) => Colors.teal.shade50),
-                    columnSpacing: 12,
-                    horizontalMargin: 12,
-                    minWidth: screenWidth,
-                    columns: dataColumn(),
-                    rows: dataRow(snapshot.data!),
+                // AllServices().toastMessage(
+                //     'Data Not found!', Colors.red, Colors.white, 16);
+                return const Text('Data Not found!');
+              } else if (snapshot.data!.promoPbList.isNotEmpty) {
+                return SingleChildScrollView(
+                  child: Padding(
+                    padding: const EdgeInsets.all(8.0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        const SizedBox(height: 5),
+                        const Text(
+                          'Bonus:',
+                          style: TextStyle(fontSize: 18),
+                        ),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 300,
+                          child: DataTable2(
+                            border: TableBorder.all(),
+                            headingRowColor: MaterialStateColor.resolveWith(
+                                (states) => Colors.blue.shade200),
+                            dataRowColor: MaterialStateColor.resolveWith(
+                                (states) => Colors.teal.shade50),
+                            columnSpacing: 12,
+                            horizontalMargin: 12,
+                            minWidth: screenWidth,
+                            columns: dataColumn(),
+                            rows: dataRow(snapshot.data!),
+                          ),
+                        ),
+                        const SizedBox(height: 5),
+                        const Text('Spcial Rate:',
+                            style: TextStyle(fontSize: 18)),
+                        const SizedBox(height: 5),
+                        SizedBox(
+                          height: 300,
+                          child: DataTable2(
+                            border: TableBorder.all(),
+                            headingRowColor: MaterialStateColor.resolveWith(
+                                (states) => Colors.blue.shade200),
+                            dataRowColor: MaterialStateColor.resolveWith(
+                                (states) => Colors.teal.shade50),
+                            columnSpacing: 12,
+                            horizontalMargin: 12,
+                            minWidth: screenWidth,
+                            columns: spDataColumn(),
+                            rows: spDataRow(snapshot.data!),
+                          ),
+                        ),
+                      ],
+                    ),
                   ),
                 );
               }
@@ -102,10 +136,12 @@ class _PromoPageState extends State<PromoPage> {
     }
   }
 
+  //==================== For Promo List================
+
   List<DataColumn> dataColumn() {
     return [
       DataColumn2(
-        fixedWidth: screenWidth / 1.30,
+        fixedWidth: screenWidth / 1.32,
         label: const Center(
           child: Text(
             'Offer',
@@ -116,7 +152,7 @@ class _PromoPageState extends State<PromoPage> {
       ),
       DataColumn2(
         numeric: true,
-        fixedWidth: screenWidth / 7.5,
+        fixedWidth: screenWidth / 7.2,
         label: const Text(
           'MinQty',
           style: TextStyle(fontSize: 16),
@@ -127,10 +163,58 @@ class _PromoPageState extends State<PromoPage> {
   }
 
   List<DataRow> dataRow(PromoModel promoData) {
-    return promoData.promoList
+    return promoData.promoPbList
         .map((e) => DataRow(cells: [
               DataCell(Text(e.promoDes)),
               DataCell(Text(e.minQty.toString()))
+            ]))
+        .toList();
+  }
+  //==================== For Promo List================
+
+  List<DataColumn> spDataColumn() {
+    return [
+      DataColumn2(
+        fixedWidth: screenWidth / 1.65,
+        label: const Center(
+          child: Text(
+            'Product',
+            style: TextStyle(fontSize: 16),
+          ),
+        ),
+        size: ColumnSize.L,
+      ),
+      DataColumn2(
+        numeric: true,
+        fixedWidth: screenWidth / 6.5,
+        label: const Text(
+          'MinQty',
+          style: TextStyle(fontSize: 16),
+        ),
+        size: ColumnSize.S,
+      ),
+      DataColumn2(
+        numeric: true,
+        fixedWidth: screenWidth / 7.2,
+        label: const Text(
+          'Rate',
+          style: TextStyle(fontSize: 16),
+        ),
+        size: ColumnSize.S,
+      ),
+    ];
+  }
+
+  List<DataRow> spDataRow(PromoModel promoData) {
+    return promoData.promoSpList
+        .map((e) => DataRow(cells: [
+              DataCell(Text(e.promoDes)),
+              DataCell(
+                Text(e.minQty.toString()),
+              ),
+              DataCell(
+                Text(e.specialRateTp.toString()),
+              )
             ]))
         .toList();
   }

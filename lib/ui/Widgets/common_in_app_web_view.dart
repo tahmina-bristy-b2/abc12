2821a6c4421bf48ed6c6637566b887b907bdf22f
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-04-25 15:00:06 +0600

    add physical back route in mobile in InAppWebView

--- lib/ui/Widgets/common_in_app_web_view.dart
+++ lib/ui/Widgets/common_in_app_web_view.dart
@@ -1,3 +1,5 @@
+import 'dart:async';
+
 import 'package:MREPORTING/services/order/order_apis.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_inappwebview/flutter_inappwebview.dart';
@@ -23,6 +25,9 @@ class CommonInAppWebView extends StatefulWidget {
 }
 
 class _CommonInAppWebViewState extends State<CommonInAppWebView> {
+  InAppWebViewController? _controller;
+  final Completer<InAppWebViewController> _controllerCompleter =
+      Completer<InAppWebViewController>();
   double progress = 0;
   String? deviceId = '';
 
@@ -35,49 +40,58 @@ class _CommonInAppWebViewState extends State<CommonInAppWebView> {
   Widget build(BuildContext context) {
     // print(
     //     '${widget.url}?cid=${widget.cid}&rep_id=${widget.userId}&rep_pass=${widget.userPassword}');
-    return Scaffold(
-        appBar: AppBar(
-          title: Text(widget.title),
-          centerTitle: true,
-          leading: IconButton(
-            onPressed: () => Navigator.pop(context),
-            icon: const Icon(Icons.home),
+    return WillPopScope(
+      onWillPop: () => _goBack(context),
+      child: Scaffold(
+          appBar: AppBar(
+            title: Text(widget.title),
+            centerTitle: true,
+            // leading: IconButton(
+            //   onPressed: () => Navigator.pop(context),
+            //   icon: const Icon(Icons.home),
+            // ),
           ),
-        ),
-        body: Stack(
-          children: [
-            Align(
-              alignment: Alignment.center,
-              child: SafeArea(
-                child: Center(
-                  child: InAppWebView(
-                    initialUrlRequest: URLRequest(
-                        url: Uri.parse(widget.clientId != null
-                            ? OrderApis.orderDynamicReportApi(
-                                widget.url,
-                                widget.cid,
-                                widget.userId,
-                                widget.userPassword,
-                                widget.clientId!)
-                            : "${widget.url}?cid=${widget.cid}&rep_id=${widget.userId}&rep_pass=${widget.userPassword}")),
-                    onReceivedServerTrustAuthRequest:
-                        (controller, challenge) async {
-                      return ServerTrustAuthResponse(
-                          action: ServerTrustAuthResponseAction.PROCEED);
-                    },
-                    onProgressChanged:
-                        (InAppWebViewController controller, int progress) {
-                      setState(() {
-                        this.progress = progress / 100;
-                      });
-                    },
+          body: Stack(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: SafeArea(
+                  child: Center(
+                    child: InAppWebView(
+                      initialUrlRequest: URLRequest(
+                          url: Uri.parse(widget.clientId != null
+                              ? OrderApis.orderDynamicReportApi(
+                                  widget.url,
+                                  widget.cid,
+                                  widget.userId,
+                                  widget.userPassword,
+                                  widget.clientId!)
+                              : "${widget.url}?cid=${widget.cid}&rep_id=${widget.userId}&rep_pass=${widget.userPassword}")),
+                      onReceivedServerTrustAuthRequest:
+                          (controller, challenge) async {
+                        return ServerTrustAuthResponse(
+                            action: ServerTrustAuthResponseAction.PROCEED);
+                      },
+                      onWebViewCreated:
+                          (InAppWebViewController webViewController) {
+                        _controllerCompleter.future
+                            .then((value) => _controller = value);
+                        _controllerCompleter.complete(webViewController);
+                      },
+                      onProgressChanged:
+                          (InAppWebViewController controller, int progress) {
+                        setState(() {
+                          this.progress = progress / 100;
+                        });
+                      },
+                    ),
                   ),
                 ),
               ),
-            ),
-            Align(alignment: Alignment.topCenter, child: _buildProgressBar()),
-          ],
-        ));
+              Align(alignment: Alignment.topCenter, child: _buildProgressBar()),
+            ],
+          )),
+    );
   }
 
   Widget _buildProgressBar() {
@@ -90,5 +104,33 @@ class _CommonInAppWebViewState extends State<CommonInAppWebView> {
       );
     }
     return Container();
+  }
+
+  Future<bool> _goBack(BuildContext context) async {
+    if (await _controller!.canGoBack()) {
+      _controller!.goBack();
+      return Future.value(false);
+    } else {
+      // showDialog(
+      //     context: context,
+      //     builder: (context) => AlertDialog(
+      //           title: Text('Do you want to exit'),
+      //           actions: <Widget>[
+      //             ElevatedButton(
+      //               onPressed: () {
+      //                 Navigator.of(context).pop();
+      //               },
+      //               child: Text('No'),
+      //             ),
+      //             ElevatedButton(
+      //               onPressed: () {
+      //                 SystemNavigator.pop();
+      //               },
+      //               child: Text('Yes'),
+      //             ),
+      //           ],
+      //         ));
+      return Future.value(true);
+    }
   }
 }

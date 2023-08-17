29ce39ca6cfaaeb985b5bb10d327ec2d1a7dbe54
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-08-17 09:58:41 +0600

    version change

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,6 +1,6 @@
 const appVersion = 'v04'; //app identify version
 const loginPageVersionName =
-    "$appVersion-20230503"; //this variable used in login page bottom secttion
+    "$appVersion-20230608"; //this variable used in login page bottom secttion
 // String cid = '';
 String userId = '';
 String userPassword = '';

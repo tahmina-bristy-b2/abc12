dba1c646cfa63655707e2b3bb5b76c538715e105
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-09-12 11:49:31 +0600

    build time version changed

--- lib/utils/constant.dart
+++ lib/utils/constant.dart
@@ -1,10 +1,9 @@
 const appVersion = 'v04'; //app identify version
 const loginPageVersionName =
-    "$appVersion-202311091421"; //this variable used in login page bottom secttion
+    "$appVersion-202309121148"; //this variable used in login page bottom secttion
 // String cid = '';
 String userId = '';
 String userPassword = '';
 List result = [];
 String interNetErrorMsg =
     'No Internet Connection\nPlease check your internet connection.';
-    

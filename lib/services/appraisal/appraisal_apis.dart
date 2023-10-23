bfcae9f92cdbb7ac85cf11257d6a8fc5813280b5
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-10-23 12:11:55 +0600

    Assined model

--- lib/services/appraisal/appraisal_apis.dart
+++ lib/services/appraisal/appraisal_apis.dart
@@ -1,4 +1,5 @@
 class AppraisalApis {
-  static employeeApi(String cid, String userId, String usrPass) =>
-      'http://127.0.0.1:8000/skf_api/api_appraisal_skf/get_area_rep?cid=$cid&user_id=$userId&user_pass=$usrPass';
+  static employeeApi(
+          String syncUrl, String cid, String userId, String usrPass) =>
+      '${syncUrl}api_appraisal_skf/get_area_rep?cid=$cid&user_id=$userId&user_pass=$usrPass';
 }

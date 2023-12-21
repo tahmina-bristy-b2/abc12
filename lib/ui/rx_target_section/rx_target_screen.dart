2f95b7d36fa8d826ce54cea7e5ffa50ce30043bc
Md. Moniruzzaman <ammoniruzzaman@gmail.com>
2023-12-21 11:19:41 +0600

    Dcr Rx Target to save

--- lib/ui/rx_target_section/rx_target_screen.dart
+++ lib/ui/rx_target_section/rx_target_screen.dart
@@ -1,19 +1,22 @@
 import 'package:MREPORTING/local_storage/boxes.dart';
 import 'package:MREPORTING/models/hive_models/dmpath_data_model.dart';
+import 'package:MREPORTING/models/hive_models/hive_data_model.dart';
 import 'package:MREPORTING/models/hive_models/login_user_model.dart';
 import 'package:MREPORTING/services/all_services.dart';
+import 'package:MREPORTING/ui/homePage.dart';
+import 'package:MREPORTING/utils/constant.dart';
 import 'package:flutter/material.dart';
+import 'package:internet_connection_checker/internet_connection_checker.dart';
 
 class RxTargetScreen extends StatefulWidget {
   final List syncDoctorList;
-  const RxTargetScreen({super.key,required this.syncDoctorList});
+  const RxTargetScreen({super.key, required this.syncDoctorList});
 
   @override
   State<RxTargetScreen> createState() => _RxTargetScreenState();
 }
 
 class _RxTargetScreenState extends State<RxTargetScreen> {
-
   UserLoginModel? userLoginInfo;
   final _formkey = GlobalKey<FormState>();
   Map<String, TextEditingController> controllers = {};
@@ -21,7 +24,10 @@ class _RxTargetScreenState extends State<RxTargetScreen> {
   final TextEditingController searchController2 = TextEditingController();
 
   DmPathDataModel? dmpathData;
+  final dcrRxTagetSavedBox = Boxes.dcrUsers();
+  List<DcrDataModel> dcrRxTargetValueInputedList = [];
 
+  int _currentSelected = 2;
   List foundUsers = [];
   var orderamount = 0.0;
   var neworderamount = 0.0;
@@ -57,6 +63,41 @@ class _RxTargetScreenState extends State<RxTargetScreen> {
     super.initState();
   }
 
+  _onItemTapped(int index) async {
+    if (index == 0) {
+      toSaveRxTargetValue();
+      Navigator.pop(context);
+      setState(() {
+        _currentSelected = index;
+      });
+    }
+
+    // if (index == 2) {
+    //   setState(() {
+    //     _isLoading = false;
+    //   });
+    //   bool result = await InternetConnectionChecker().hasConnection;
+    //   if (result == true) {
+    //     // dcrGSPSubmit();
+    //   } else {
+    //     AllServices()
+    //         .toastMessage(interNetErrorMsg, Colors.red, Colors.white, 16);
+    //     setState(() {
+    //       _isLoading = true;
+    //     });
+    //     // print(InternetConnectionChecker().lastTryResults);
+    //   }
+
+    //   setState(() {
+    //     _currentSelected = index;
+    //   });
+    // }
+  }
+
+  Future toSaveRxTargetValue() async {
+    // dcrRxTagetSavedBox.put('dcrRxTargetValue', dcrRxTargetValueInputedList);
+  }
+
   @override
   void dispose() {
     for (var element in foundUsers) {
@@ -67,43 +108,60 @@ class _RxTargetScreenState extends State<RxTargetScreen> {
     super.dispose();
   }
 
-
-
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-
       appBar: AppBar(
         backgroundColor: const Color.fromARGB(255, 138, 201, 149),
-      title: const Text('RX Target'),
-      titleTextStyle: const TextStyle(
-          color: Color.fromARGB(255, 27, 56, 34),
-          fontWeight: FontWeight.w500,
-          fontSize: 20),
-      centerTitle: true,
+        title: const Text('RX Target'),
+        titleTextStyle: const TextStyle(
+            color: Color.fromARGB(255, 27, 56, 34),
+            fontWeight: FontWeight.w500,
+            fontSize: 20),
+        centerTitle: true,
       ),
-
-     
-        body: Column(
-          children: [
-            itemSearchTextFormWidget(),
-            //Expanded(child: itemSearchTextFormWidget()),
-            itemListViewBuilderWIdget(),
-            const SizedBox(
-              height: 5,
+      bottomNavigationBar: BottomNavigationBar(
+        type: BottomNavigationBarType.fixed,
+        onTap: _onItemTapped,
+        currentIndex: _currentSelected,
+        showUnselectedLabels: true,
+        unselectedItemColor: Colors.grey[800],
+        selectedItemColor: const Color.fromRGBO(10, 135, 255, 1),
+        backgroundColor: Colors.white,
+        items: const <BottomNavigationBarItem>[
+          BottomNavigationBarItem(
+            label: 'Save Drafts',
+            icon: Icon(Icons.drafts),
+          ),
+          BottomNavigationBarItem(
+            label: '',
+            icon: Icon(
+              Icons.clear,
+              color: Color.fromRGBO(255, 254, 254, 1),
             ),
-           // addtoCartButtonWidget(context)
-          ],
-        ),
-  
-
+          ),
+          BottomNavigationBarItem(
+            label: 'Submit',
+            icon: Icon(Icons.save),
+          ),
+        ],
+      ),
+      body: Column(
+        children: [
+          itemSearchTextFormWidget(),
+          //Expanded(child: itemSearchTextFormWidget()),
+          itemListViewBuilderWIdget(),
+          const SizedBox(
+            height: 5,
+          ),
+          // addtoCartButtonWidget(context)
+        ],
+      ),
     );
   }
 
-
   //==================================================== doctor seacrch===================================
-   SizedBox itemSearchTextFormWidget() {
+  SizedBox itemSearchTextFormWidget() {
     return SizedBox(
       height: 60,
       child: Row(
@@ -277,27 +335,27 @@ class _RxTargetScreenState extends State<RxTargetScreen> {
                                 //               fontSize: 14),
                                 //         ),
                                 //       )
-                                    // ? Padding(
-                                    //     padding: const EdgeInsets.all(4.0),
-                                    //     child: AnimatedTextKit(
-                                    //       repeatForever: true,
-                                    //       animatedTexts: [
-                                    //         ColorizeAnimatedText(
-                                    //           foundUsers[index]['promo'],
-                                    //           textStyle: const TextStyle(
-                                    //               color: Color.fromARGB(
-                                    //                   255, 8, 18, 20),
-                                    //               fontSize: 15),
-                                    //           colors: [
-                                    //             const Color.fromARGB(
-                                    //                 255, 18, 137, 235),
-                                    //             Colors.red
-                                    //           ],
-                                    //         ),
-                                    //       ],
-                                    //     ),
-                                    //   )
-                                    // : Container(),
+                                // ? Padding(
+                                //     padding: const EdgeInsets.all(4.0),
+                                //     child: AnimatedTextKit(
+                                //       repeatForever: true,
+                                //       animatedTexts: [
+                                //         ColorizeAnimatedText(
+                                //           foundUsers[index]['promo'],
+                                //           textStyle: const TextStyle(
+                                //               color: Color.fromARGB(
+                                //                   255, 8, 18, 20),
+                                //               fontSize: 15),
+                                //           colors: [
+                                //             const Color.fromARGB(
+                                //                 255, 18, 137, 235),
+                                //             Colors.red
+                                //           ],
+                                //         ),
+                                //       ],
+                                //     ),
+                                //   )
+                                // : Container(),
                               ],
                             ),
                           ),
@@ -329,7 +387,33 @@ class _RxTargetScreenState extends State<RxTargetScreen> {
                                         border: OutlineInputBorder(),
                                       ),
                                       onChanged: (value) {
-                                       // itemCount(value, index);
+                                        // itemCount(value, index);
+                                        // Thats Added for Save
+                                        if (value.isNotEmpty && value != '') {
+                                          var temp = DcrDataModel(
+                                              docName: foundUsers[index]
+                                                  ['doc_name'],
+                                              docId: foundUsers[index]
+                                                  ['doc_id'],
+                                              areaId: foundUsers[index]
+                                                  ['doc_id'],
+                                              areaName: foundUsers[index]
+                                                  ['doc_id'],
+                                              address: 'address',
+                                              dcrGspList: [],
+                                              visitedWith: '',
+                                              notes: '',
+                                              rxTargetValue: controllers[
+                                                      foundUsers[index]
+                                                          ['doc_id']]!
+                                                  .text);
+                                          dcrRxTargetValueInputedList
+                                              .removeWhere((element) =>
+                                                  element.docId ==
+                                                  foundUsers[index]['doc_id']);
+
+                                          dcrRxTargetValueInputedList.add(temp);
+                                        }
                                       },
                                     ),
                                   ),
@@ -349,5 +433,4 @@ class _RxTargetScreenState extends State<RxTargetScreen> {
       ),
     );
   }
-
 }
